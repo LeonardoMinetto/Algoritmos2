ef14c9f4f1dc46c5f373f1d130ce0ce3d409e29a
Leonardo Minetto <leonardominetto3@gmail.com>
2025-05-22 12:29:42 -0300

    Inverte listas encadeadas

---
+++ InverteListaEncadeada.c
@@ -0,0 +1,35 @@
+void inverteLista(ListaE *l){
+    Cell *aux1, *aux2=NULL;
+    if(l == NULL){
+        aux1=l->head;
+        while(aux1!=NULL){
+            l->head = aux1->next;
+            aux1->next = aux2;
+            aux2=aux1;
+            aux1=l->head;
+        }
+        l->head = aux2;
+    }
+}
+int main(){
+    ListaE *l = criar_listaE();
+    int num;
+    while(num != -1){
+        scanf("%d", &num);
+        if(num == -1){
+            break;
+        }
+        inserir_primeiro(num, l);
+        
+    }
+    inverteLista(l);
+    imprimir(l);
+    Cell *aux=l->head;
+    Cell *aux2;
+    while(aux2!=NULL){
+        aux2 = aux;
+        aux = aux->next;
+        free(aux2);
+    }
+
+}
